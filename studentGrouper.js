bb10204965b32a2edd6111c7da3bfff42807a820
Sophie DeBenedetto <robin@example.com>
2019-01-16 18:27:06 +0000

    testing

---
+++ studentGrouper.js
@@ -0,0 +1,5 @@
+students = ["Lisa", "Jacob", "Hans", "Donna", "Sid", "Jeffrey", "CJ", "Christian", "Fiola", "Lou", "Mallory", "Carl", "John"]
+
+students.forEach(function(student) {
+  console.log(student)
+})
